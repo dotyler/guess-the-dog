8fec05896d2e11f519f6d6e43833dd890bbb2875
dotyler <dotyler@yahoo.com>
2023-07-22 19:12:24 -0500

    added code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import {dogImages, dogImageAnswers} from "./DogImages.js"
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [answers, setAnswers] = React.useState([""]) 
+
+  let index = Math.floor(Math.random() * dogImages.length)
+  let randomImage = dogImages[index];
+
+  React.useEffect(() => {
+
+    // index = Math.floor(Math.random() * dogImages.length)
+    // randomImage = dogImages[index]
+    setAnswers([dogImageAnswers[index], dogImageAnswers[Math.floor(Math.random() * dogImages.length)],dogImageAnswers[Math.floor(Math.random() * dogImages.length)]].sort(()=> 0.5 - Math.random()))
+
+  },[])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={randomImage} className="App-logo" alt="logo" />
+        <p>what dog is this</p>
+        <div>
+          {answers.map((answer) => (
+            <button key={answer}>{answer}</button>
+          ))}
+        </div>
       </header>
     </div>
   );
---
+++ src/DogImages.js
@@ -0,0 +1,26 @@
+
+export const dogImages =
+[
+  require('./img/image1.jpg'),
+  require('./img/image2.jpg'),
+  require('./img/aliendog.jpg'),
+  require('./img/balloondog.jpg'),
+  require('./img/bathdog.jpg'),
+  require('./img/cooldog.jpg'),
+  require('./img/skydog.jpg')
+];
+
+export const dogImageAnswers =
+[
+  "Angry",
+  "Hungry",
+  "Alien",
+  "Balloon",
+  "Bath",
+  "Cool",
+  "Sky"
+];
+
+
+
+// export default dogImages;
